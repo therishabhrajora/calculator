c621442a8e7418a1dd8b1ce7b2d5d97fb81754ee
therishabhrajora <rishabhrajora20@gmail.com>
2024-03-31 11:58:16 +0530

    modified

--- calculator-react/src/components/buttonContainer.jsx
+++ calculator-react/src/components/buttonContainer.jsx
@@ -1,11 +1,11 @@
 import styles from '../modules/buttonContainer.module.css'
-import Buttonitem from './button.jsx'
 
-let ButtonConatiner = ({values}) => {
-    
+let ButtonConatiner = ({onbuttonclick}) => {
+    const buttons = ['C', '1', '2', '*', '3', '4', '/', '6', '7', '+', '8', '9', '0', '='];
+
     return <div className={styles.buttonsContainer}>
-        {values.map(button => (
-            <Buttonitem key={button} buttons={button}></Buttonitem>
+        {buttons.map((button) => (
+            <button key={button }className={styles.button} onClick={()=>onbuttonclick(button)}>{button}</button>
         ))}
     </div>
 }
